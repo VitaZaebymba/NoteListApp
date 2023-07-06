967d87d05162df9100ab680734b28b569ff1df68
Vita <malavita29@mail.ru>
2023-07-06 21:20:51 +0300

    add NoteFragment

---
+++ app/src/main/java/com/vita_zaebymba/notelistapp/fragments/NoteFragment.kt
@@ -0,0 +1,28 @@
+package com.vita_zaebymba.notelistapp.fragments
+
+import android.os.Binder
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.vita_zaebymba.notelistapp.R
+import com.vita_zaebymba.notelistapp.databinding.FragmentNoteBinding
+
+class NoteFragment: Fragment() {
+
+    private lateinit var binding: FragmentNoteBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = NoteFragment()
+    }
+}
