0994787b53018892bb78137e5722737ed6c4d870
Vita <malavita29@mail.ru>
2023-07-06 15:22:57 +0300

    add fragment

---
+++ app/src/main/java/com/vita_zaebymba/notelistapp/fragments/FragmentManager.kt
@@ -0,0 +1,15 @@
+package com.vita_zaebymba.notelistapp.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import com.vita_zaebymba.notelistapp.R
+
+object FragmentManager {
+    var currentFragment: BaseFragment?= null // используется для отслеживания текущего активного фрагмента
+
+    fun setFragment(newFrag: BaseFragment, activity: AppCompatActivity) {
+        val transaction = activity.supportFragmentManager.beginTransaction() // создает новую транзакцию фрагмента
+        transaction.replace(R.id.placeHolder, newFrag) // указывается, что текущий фрагмент должен быть заменен на новый фрагмент
+        transaction.commit()
+        currentFragment = newFrag
+    }
+}
