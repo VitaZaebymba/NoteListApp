21545112a020af2b538a83e12f76cac76af2a881
Vita <malavita29@mail.ru>
2023-07-06 14:28:32 +0300

    add onClick for bottom menu

--- app/src/main/java/com/vita_zaebymba/notelistapp/activities/MainActivity.kt
+++ app/src/main/java/com/vita_zaebymba/notelistapp/activities/MainActivity.kt
@@ -2,6 +2,9 @@ package com.vita_zaebymba.notelistapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.vita_zaebymba.notelistapp.R
 import com.vita_zaebymba.notelistapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setBottomNavListener()
     }
+
+    private fun setBottomNavListener() {
+        binding.bNav.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.settings -> {
+                    Toast.makeText(applicationContext, "Settings clicked!", Toast.LENGTH_SHORT).show()
+                }
+                R.id.notes -> {
+                    Toast.makeText(applicationContext, "Notes clicked!", Toast.LENGTH_SHORT).show()
+                }
+                R.id.shop_list -> {
+                    Toast.makeText(applicationContext, "List clicked!", Toast.LENGTH_SHORT).show()
+                }
+                R.id.new_item -> {
+                    Toast.makeText(applicationContext, "New item clicked!", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
+    }
+
 }
